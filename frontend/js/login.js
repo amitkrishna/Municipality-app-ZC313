4c5ed3816ac29fccf3f9faaede16bf0da2e2ebb8
I516384 <rahul.agarwal06@sap.com>
2020-05-26 22:39:38 +0530

    Basic UI for login/register > FRONTEND

---
+++ frontend/js/login.js
@@ -0,0 +1,25 @@
+$("#login").click(() => {
+    activity.start();
+    if (loginDetails.isthere()) {
+        if(loginDetails.checkEmail() != "isNotValid"){
+            
+            // TODO AJAX Call to backend
+        } else {
+            messageBox.show("Email is Invalid");
+        }
+    } else {
+        messageBox.show("Fields are empty");
+    }
+});
+
+loginDetails = () => {};
+loginDetails.isthere = () => {
+    if ($("#email").val().trim() == "" || $("#password").val().trim() == "")
+        return false;
+    return true;
+};
+loginDetails.checkEmail = () => {
+    if(isEmail($("#email").val().trim()))
+        return $("#email").val().trim()
+    return "isNotValid";
+};
